777cf7f0b0f8c47e9c498f1257b6f7563bee3d2c
CrCbabe <crcobabe@gmail.com>
2023-01-07 15:02:07 -0700

    Completed C# Prep parts 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        string letterGrade = "";
+        string message = "";
+
+        if (percent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (percent >= 70)
+        {
+            message = "Congratulations on passing this course!";
+        }
+        else
+        {
+            message = "Better luck next time!";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}. {message}");
     }
 }
