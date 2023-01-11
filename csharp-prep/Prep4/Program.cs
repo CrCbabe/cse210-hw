65056faabb3cad4a942acf7fe2b3fdf4b257f1a3
CrCbabe <crcobabe@gmail.com>
2023-01-11 11:10:34 -0700

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.WriteLine("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        while (number != 0) {
+            numbers.Add(number);
+            Console.WriteLine("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine($"The sum is: " + numbers.AsEnumerable().Sum());
+        Console.WriteLine("The average is: " + Queryable.Average(numbers.AsQueryable()));
+        Console.WriteLine("The largest number is: " + numbers.Max());
     }
 }
