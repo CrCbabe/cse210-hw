f46bedd533ceea6ee5e92cdcdcd8f23baefde45d
CrCbabe <crcobabe@gmail.com>
2023-01-25 13:39:34 -0700

    Created classes

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,8 @@
+using System;
+
+class Entry {
+    public List prompts = ["What was the strongest emotion I felt today?","How did I see the hand of the Lord in my life today?","What did I do today that I am proud of?","If I could have done something better today, what would it be?","What can I be grateful for today?","Who inspired me today?"];
+    public string prompt = 
+    public string response = Console.ReadLine();
+     
+}
