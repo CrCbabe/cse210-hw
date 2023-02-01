c199ae0b582992b0aa2c4f6cb6dde3abec8df1ed
CrCbabe <crcobabe@gmail.com>
2023-02-01 11:04:10 -0700

    added date functionality to entry

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -9,11 +9,12 @@ class Entry
             "How did I see the hand of the Lord in my life today?",
             "What did I do today that I am proud of?",
             "If I could have done something better today, what would it be?",
-            "What can I be grateful for today?", "Who inspired me today?"
+            "What can I be grateful for today?", 
+            "Who inspired me today?"
         ];
     var random = new Random();
     int index = random.Next(prompts.Count);
     public string prompt = Console.WriteLine(prompts[index]);
     public string response = Console.ReadLine();
-
+    string date = DateTime.Now.ToString("mm:dd:yyyy");
 }
