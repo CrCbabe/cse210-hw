61d2ac241ba3bde28f3ee01249c4397434d4a2ef
CrCbabe <crcobabe@gmail.com>
2023-02-04 14:54:49 -0700

    tested out lists

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        List<string> AuthorList = new List<string>();
+        AuthorList.Add("Mahesh Chand");
+        AuthorList.Add("Praveen Kumar");
+        AuthorList.Add("Raj Kumar");
+        AuthorList.Add("Nipun Tomar");
+        AuthorList.Add("Dinesh Beniwal");
+        Console.WriteLine(AuthorList);
     }
 }
