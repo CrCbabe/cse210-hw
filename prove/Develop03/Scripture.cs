b0d756c6f59da0fee4f259465699ef0aec5ca0a5
CrCbabe <crcobabe@gmail.com>
2023-02-15 12:03:27 -0700

    Initialized Reference into Scripture

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -2,6 +2,17 @@ using System;
 
 class Scripture
 {
+    static Reference chosenReference = new Reference();
+    static string chosenScripture = chosenReference.ChooseScripture();
+    static string[] _scripture = chosenScripture.Split("~");
+    static string _reference = _scripture[0];
+    static string _verse = _scripture[1];
+
+    public void MemorizeScripture()
+    {
+        Console.WriteLine("");
+    }
+
     //fix private void later to appropriate
     private void HideWords()
     {
@@ -10,11 +21,11 @@ class Scripture
 
     private void GetRenderedText()
     {
-
+        string renderedText = new Word.();
     }
 
     private void IsCompletelyHidden()
     {
-
+        Environment.Exit(0);
     }
 }
