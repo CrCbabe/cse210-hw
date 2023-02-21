feaa8e23b04b94d0c680891589cb2e67b59def51
CrCbabe <crcobabe@gmail.com>
2023-02-21 15:25:48 -0700

    Moved UI to Program to clean Scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,6 +3,19 @@ class RunProgram
     static void Main(string[] args)
     {
         Scripture scripture = new Scripture();
-        scripture.Memorize();
+        scripture.DisplayScripture();
+        Console.WriteLine("Press Enter to continue, or type 'quit' to finish");
+        string _input = Console.ReadLine();
+        while (_input != "quit")
+        {
+            Console.Clear();
+            scripture.HideWords();
+            scripture.DisplayScripture();
+            // _verse = HideWords(_verse);
+            // Console.WriteLine($"{_reference}: {_verse}"); // hopefully this works
+
+            Console.WriteLine("Press Enter to continue, or type 'quit' to finish");
+            _input = Console.ReadLine();
+        }
     }
 }
