ab7528dcb51200aee4db1565ad890a17728538f3
CrCbabe <crcobabe@gmail.com>
2023-02-27 13:02:42 -0700

    Learning assignment complete

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,8 +2,28 @@ using System;
 
 class Program
 {
+    private static int choice;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("1. Start breathing activity");
+        Console.WriteLine("2. Start reflecting activity");
+        Console.WriteLine("3. Start listing activity");
+        Console.WriteLine("4. Quit");
+        while (choice != 4)
+        {
+            if (choice == 1)
+            {
+                Breather breather = new Breather();
+            }
+            else if (choice == 2)
+            {
+                Reflect reflect = new Reflect();
+            }
+            else if (choice == 3)
+            {
+                Listing list = new Listing();
+            }
+        }
     }
 }
