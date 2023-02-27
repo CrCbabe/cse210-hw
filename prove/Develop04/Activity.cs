db44cf9f9a80f7ab679f00d0a1e1afe9d7a9b84f
CrCbabe <crcobabe@gmail.com>
2023-02-27 13:27:25 -0700

    Removed Animation, inserted function to Activity

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,6 +1,29 @@
 using System;
+using System.Threading;
 
 class Activity
 {
+    public void Timer(int time)
+    {
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(time);
 
+        Thread.Sleep(3000);
+
+        DateTime currentTime = DateTime.Now;
+        if (currentTime < futureTime)
+        {
+            Console.WriteLine("You have completed the activity");
+        }
+    }
+
+    public void PlayAnimation()
+    {
+        foreach (_frame in _frames)
+        {
+            Console.Write(_frame);
+            Thread.Sleep(500);
+            Console.Write("\b \b");
+        }
+    }
 }
