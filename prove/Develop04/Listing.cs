b8ebc7577dedc0f71d61a2be91444f9225eb77dd
CrCbabe <crcobabe@gmail.com>
2023-03-01 13:31:20 -0700

    Set welcome and description using polymorphism

--- prove/Develop04/Listing.cs
+++ prove/Develop04/Listing.cs
@@ -1,7 +1,19 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 class Listing : Activity
 {
+    private string _activity = "Listing";
+    List<string> prompts = new List<string>();
 
+    public override void SetWelcome()
+    {
+        _welcome = "Welcome to the Listing Activity";
+    }
+
+    public override void SetDescription()
+    {
+        _description = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
+    }
 }
--- prove/Develop04/Reflect.cs
+++ prove/Develop04/Reflect.cs
@@ -1,7 +1,19 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 class Reflect : Activity
 {
+    private string _activity = "Reflecting";
+    List<string> prompts = new List<string>();
 
+    public override void SetWelcome()
+    {
+        _welcome = "Welcome to the Reflection Activity";
+    }
+
+    public override void SetDescription()
+    {
+        _description = "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
+    }
 }
