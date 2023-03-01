25f6881120789d6f21166b90673482df39508c3a
CrCbabe <crcobabe@gmail.com>
2023-03-01 13:26:44 -0700

    Set welcome and description w/ polymorphism

--- prove/Develop04/Breather.cs
+++ prove/Develop04/Breather.cs
@@ -3,5 +3,21 @@ using System.Threading;
 
 class Breather : Activity
 {
+    private string _activity = "Breathing";
+    public Breather()
+    {
+        int time = Welcome();
+
+    }
+
+    public override void SetWelcome()
+    {
+        _welcome = "Welcome to the Breathing Activity";
+    }
+
+    public override void SetDescription()
+    {
+        _description = "This activity will help you relax by walking through breathing in and our slowly. Clear you mind and focus on your breathing.";
+    }
 
 }
