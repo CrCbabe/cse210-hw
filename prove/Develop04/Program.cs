c8a0f50b4ab539d06b71015f95e829fa6464c5a6
CrCbabe <crcobabe@gmail.com>
2023-03-04 13:52:10 -0700

    Updated menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -9,10 +9,10 @@ class Program
         {
             Console.Clear();
             Console.WriteLine("Menu Options:");
-            Console.WriteLine("1. Start breathing activity");
-            Console.WriteLine("2. Start reflecting activity");
-            Console.WriteLine("3. Start listing activity");
-            Console.WriteLine("4. Quit");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
             Console.Write("Select a choice from the menu: ");
             choice = int.Parse(Console.ReadLine());
             if (choice == 1)
